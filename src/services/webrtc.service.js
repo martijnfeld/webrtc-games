a1fe396722b8dc607916b64b7d5412ed0a6610d2
Martijn <martijnfeld@live.nl>
2021-01-03 15:25:16 +0100

    commit

---
+++ src/services/webrtc.service.js
@@ -0,0 +1,6 @@
+import Peer from 'peerjs'; 
+// const myPeer = new Peer(undefined, {
+//     host: '/',
+//     port: '3001'
+// })
+const myPeer = new Peer();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import WebRtcService from "@/services/webrtc.service"
 
 export default {
   name: "Home",
